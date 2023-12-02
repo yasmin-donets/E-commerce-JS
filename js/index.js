a081368376e96613013871a87cdf2e6e628cc743
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-12-02 21:45:04 +0100

    fetch products from url

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ import './toggleSidebar.js';
 import './cart/cartToggle.js';
 // specific imports
 import { getElement } from './utils.js';
+import fetchProducts from './fetchproducts.js';
+
+const init = async () => {
+	const products = await fetchProducts();
+	console.log(products);
+};
+window.addEventListener('DOMContentLoaded', init);
