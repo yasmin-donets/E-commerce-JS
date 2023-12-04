2608079bfd5f6f7edae5dcc1c757f19746ecce65
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-12-05 00:02:35 +0100

    price filter

---
+++ js/filters/price.js
@@ -0,0 +1,39 @@
+import { getElement } from '../utils.js';
+import display from '../displayProducts.js';
+const setupPrice = (store) => {
+  const priceInput = getElement('.price-filter');
+  const priceValue = getElement('.price-value');
+
+  // Find the maximum price in the store
+  const maxPrice = Math.max(
+    ...store.map((product) => parseFloat(product.price))
+  );
+
+  // Set up the filter
+  priceInput.value = maxPrice;
+  priceInput.max = maxPrice;
+  priceInput.min = 0;
+  priceValue.textContent = `Value : $${maxPrice}`;
+
+  priceInput.addEventListener('input', function () {
+    const value = parseFloat(priceInput.value);
+    priceValue.textContent = `Value : $${value}`;
+
+    // Normalize values for filtering
+    const normalizedValue = value * (maxPrice / 1000);
+
+    let newStore = store.filter(
+      (product) => parseFloat(product.price) <= normalizedValue
+    );
+    display(newStore, getElement('.products-container'), true);
+
+    if (newStore.length < 1) {
+      const products = getElement('.products-container');
+      products.innerHTML = `<h3 class="filter-error">Sorry, no products matched your search</h3>`;
+    }
+  });
+	console.log(priceInput.value);
+};
+
+
+export default setupPrice;
--- js/pages/products.js
+++ js/pages/products.js
@@ -3,10 +3,12 @@
 import '../toggleSidebar.js';
 import '../cart/cartToggle.js';
 import '../cart/setupCart.js';
+
 //  filter imports
 
 import setupCategories from '../filters/categories.js';
 import setupSearch from '../filters/search.js';
+import setupPrice from '../filters/price.js';
 
 //specific imports
 
@@ -14,11 +16,13 @@ import { store } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
 
+
 const loading = getElement('.page-loading');
 display(store, getElement('.products-container'));
 
-// filters 
+// filters
 setupSearch(store);
 setupCategories(store);
+setupPrice(store);
 
 loading.style.display = 'none';
